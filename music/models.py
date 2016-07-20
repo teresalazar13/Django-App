baee291224233f1e7b545146d106480570f0970f
teresalazar13 <teresa.sal13@gmail.com>
2016-07-20 21:37:19 +0100

    start working on the database API

--- music/models.py
+++ music/models.py
@@ -7,8 +7,14 @@ class Album(models.Model):
     genre = models.CharField(max_length=30)
     album_logo = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.album_title + ', ' + self.artist + ", " + self.genre
+
 
 class Song(models.Model):
     song_title = models.CharField(max_length=50)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.song_title + ", " + self.album
