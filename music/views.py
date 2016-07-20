d567a120ab59929dbee4811218fd11e6338339b7
teresalazar13 <teresa.sal13@gmail.com>
2016-07-20 23:34:50 +0100

    create view with details of each album.

--- music/views.py
+++ music/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is the music app homepage</h1>")
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for album " + str(album_id) + "</h2> ")
