227439b523d8ece991b167a64a1e8d32fd4a7600
teresalazar13 <teresa.sal13@gmail.com>
2016-07-22 17:24:28 +0100

    create template for music's index.

--- music/views.py
+++ music/views.py
@@ -1,16 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    html = '<h1>This is the music app homepage</h1>'
-
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album " + str(album_id) + "</h2> ")
+    return HttpResponse("<h2>Details for album " + str(album_id) + "</h2>")
