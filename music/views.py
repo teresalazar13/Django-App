1007837be860b8c7c708cf04e5d1d9f0da94eacc
teresalazar13 <teresa.sal13@gmail.com>
2016-08-15 11:26:24 +0100

    namespace and HTTP 404 shortcut.

--- music/views.py
+++ music/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 
@@ -12,8 +11,5 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(id=album_id)
-    except Album.DoesNotExist:
-        raise Http404('Album does not exist')
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
