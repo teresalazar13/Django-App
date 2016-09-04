7a13984dd17b7f91be9bd810dbe3dd574eee1eef
teresalazar13 <teresa.sal13@gmail.com>
2016-09-05 00:18:40 +0100

    accounts and users.

--- music/urls.py
+++ music/urls.py
@@ -6,6 +6,8 @@ app_name = 'music'
 urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(), name='index'),
+    # /music/register
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
     # /music/id/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # /music/album/add
